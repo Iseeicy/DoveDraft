754de91179a39760240992dde4c9be59868e13d5
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-11-22 10:03:06 -0600

    Worked on CharacterAgent3D.cs. Stopped bc we need entity input

--- Character/CharacterAgent3D.cs
+++ Character/CharacterAgent3D.cs
@@ -1,9 +1,25 @@
 ﻿using Godot;
+using Godot.Collections;
 
 namespace DoveDraft.Character;
 
 [Tool, GlobalClass]
 public partial class CharacterAgent3D : CharacterBody3D
 {
+    //
+    //  Exports
+    //
+
+    /// <summary>
+    /// OPTIONAL. The character that this agent represents. If not assigned, a default value will be assigned.
+    /// </summary>
+    [Export]
+    public CharacterDefinition Character { get; set; }
+
+    /// <summary>
+    /// The player scripts that define this agent's behaviour.
+    /// </summary>
+    [Export] public Array<PackedScene> AgentScripts { get; set; } = new();
     
+    // TODO - port entity input system
 }

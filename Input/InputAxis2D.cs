301f648990e5cdb456fd1df7c4863dd6360a4d71
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-11-22 10:49:11 -0600

    Ported `net.bobbo.entity-input` from net5 to C#

---
+++ Input/InputAxis2D.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace DoveDraft.Input;
+
+[Tool, GlobalClass]
+public partial class InputAxis2D : RefCounted
+{
+    //
+    //  Public Variables
+    //
+
+    /// <summary>
+    /// The x-axis of this 2D axis.
+    /// </summary>
+    public InputAxis1D X { get; set; } = new();
+    
+    /// <summary>
+    /// The y-axis of this 2D axis.
+    /// </summary>
+    public InputAxis1D Y { get; set; } = new();
+    
+    //
+    //  Godot Methods
+    //
+
+    public InputAxis2D() { }
+    
+    public InputAxis2D(InputAxis1D x, InputAxis1D y)
+    {
+        X = x;
+        Y = y;
+    }
+}

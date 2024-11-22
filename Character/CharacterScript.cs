59d68c3928cd5e0b1e8fd8f70f45b4da6ece6d4c
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-11-22 09:59:07 -0600

    Implemented character scripts

--- Character/CharacterScript.cs
+++ Character/CharacterScript.cs
@@ -5,5 +5,78 @@ namespace DoveDraft.Character;
 [Tool, GlobalClass]
 public partial class CharacterScript : Node
 {
+    //
+    //  Public Variables
+    //
     
+    /// <summary>
+    /// The CharacterAgent that this script belongs to. Can be `CharacterAgent3D`, `CharacterAgent2D`, or null.
+    /// </summary>
+    public Node Agent => Agent3D != null ? Agent3D : Agent2D;
+
+    /// <summary>
+    /// The 3D agent that we belong to, if there is one.
+    /// </summary>
+    public CharacterAgent3D Agent3D => Owner as CharacterAgent3D;
+
+    /// <summary>
+    /// The 2D agent that we belong to, if there is one.
+    /// </summary>
+    public CharacterAgent2D Agent2D => Owner as CharacterAgent2D;
+
+    /// <summary>
+    /// The script runner that belongs to our parent agent, if there is one.
+    /// </summary>
+    public CharacterScriptRunner ScriptRunner => Agent switch
+    {
+        CharacterAgent2D agent2d => agent2d.ScriptRunner,
+        CharacterAgent3D agent3d => agent3d.ScriptRunner,
+        _ => null
+    };
+
+    /// <summary>
+    /// Is this character script executing?
+    /// </summary>
+    public bool IsRunning
+    {
+        get => _isRunning;
+        set
+        {
+            _isRunning = value;
+            SetProcess(value);
+            SetPhysicsProcess(value);
+            SetProcessUnhandledInput(value);
+            SetBlockSignals(!value);
+        }
+    }
+    private bool _isRunning = true;
+    
+    //
+    //  Public Methods
+    //
+
+    public void CallAgentReady()
+    {
+        AgentReady();
+    }
+
+    public void CallAgentProcess(double delta)
+    {
+        if(IsRunning) AgentProcess(delta);
+    }
+
+    public void CallAgentPhysicsProcess(double delta)
+    {
+        if (IsRunning) AgentPhysicsProcess(delta);
+    }
+    
+    //
+    //  Protected Methods
+    //
+
+    protected virtual void AgentReady() { }
+    
+    protected virtual void AgentProcess(double delta) { }
+    
+    protected virtual void AgentPhysicsProcess(double delta) { }
 }
--- Character/CharacterScriptRunner.cs
+++ Character/CharacterScriptRunner.cs
@@ -1,9 +1,67 @@
 ﻿using Godot;
+using Godot.Collections;
 
 namespace DoveDraft.Character;
 
 [Tool, GlobalClass]
 public partial class CharacterScriptRunner : Node
 {
+    //
+    //  Public Variables
+    //
+
+    /// <summary>
+    /// The scripts to run. These are the children of this node.
+    /// </summary>
+    public Array<CharacterScript> Scripts { get; private set; } = new();
     
+    //
+    //  Public Methods
+    //
+
+    public void Setup()
+    {
+        Scripts.Clear();
+        FindScriptsInChildren(Scripts);
+    }
+
+    public void ScriptsReady()
+    {
+        foreach (CharacterScript script in Scripts)
+        {
+            script.CallAgentReady();
+        }
+    }
+
+    public void ScriptsProcess(double delta)
+    {
+        foreach (CharacterScript script in Scripts)
+        {
+            script.CallAgentProcess(delta);
+        }
+    }
+    
+    public void ScriptsPhysicsProcess(double delta)
+    {
+        foreach (CharacterScript script in Scripts)
+        {
+            script.CallAgentPhysicsProcess(delta);
+        }
+    }
+    
+    //
+    //  Private Methods
+    //
+
+    /// <summary>
+    /// Sort through the top level child nodes and compile a list of scripts.
+    /// </summary>
+    /// <param name="results">Where to store the found CharacterScripts.</param>
+    private void FindScriptsInChildren(Array<CharacterScript> results)
+    {
+        foreach (Node node in GetChildren())
+        {
+            if(node is CharacterScript script) results.Add(script);
+        }
+    }
 }
